dc31ea31779b8d8c2d965c8478560593395d6de2
Michal Fojtik <mfojtik@redhat.com>
2014-07-16 14:03:47 +0200

    OpenShift: Port session_store to support rails4 format

--- .openshift/lib/session_store.rb
+++ .openshift/lib/session_store.rb
@@ -3,7 +3,8 @@ require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Be sure to restart your server when you modify this file.
 
 # Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
-RailsApp::Application.config.session_store :cookie_store, :key => initialize_secret(
+
+Rails.application.config.session_store :cookie_store, :key => initialize_secret(
   :session_store,
   '_railsapp_session'
 )
