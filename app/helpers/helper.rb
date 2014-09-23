54ba5ea46501e271b2e9994f0af834a8fa4829b8
Samim Pezeshki <p.samim@gmail.com>
2014-09-23 10:36:20 +0330

    Fix bug if it is course's first lesson

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,16 +1,17 @@
 module Helper
-  def Helper.next_lesson(user, course)
-    last_quiz = Quiz.joins(:lesson).where(lessons: {course_id: course}, user_id: user).last
+  def self.next_lesson(user, course)
+    last_quiz = Quiz.joins(:lesson)
+      .where(lessons: { course_id: course }, user_id: user).last
+    return course.lessons.where(position: 1).first unless last_quiz
     last_lesson_position  = last_quiz.lesson.position
     next_lesson = Lesson.where(position: last_lesson_position + 1).first
-    return next_lesson
+    next_lesson
   end
 
-  def Helper.create_quiz(user, lesson)
-    q =Quiz.new(user: user, lesson: lesson)
+  def self.create_quiz(user, lesson)
+    q = Quiz.new(user: user, lesson: lesson)
     q.secret = Digest::SHA1.hexdigest Time.new.to_f.to_s + user.id.to_s
     q.expire = 2.day.from_now
     q.save
-    return q
   end
 end
