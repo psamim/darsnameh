2ce2cb550a8ab10b5a23ce4a330cc6b31f34bef0
Samim Pezeshki <p.samim@gmail.com>
2014-09-23 10:36:44 +0330

    Add enrollment confirmation and welcome sender

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,22 +1,18 @@
 class Mailer < ActionMailer::Base
   include Helper
 
-  default from: "samim@sandbox175821ce2bc7404e8a48d3d8b11e3630.mailgun.org"
-
-  def welcome
-    welcome_email = mail to: "psamim@gmail.com", subject: "Success! You did it."
-    welcome_email.deliver
-  end
+  default from: 'samim@sandbox175821ce2bc7404e8a48d3d8b11e3630.mailgun.org'
 
   def send_next_lesson(user, course)
     next_lesson = Helper.next_lesson user, course
     next_lesson_mail = mail to: user.email, subject: next_lesson.title
+    @lesson = next_lesson
     next_lesson_mail.deliver
   end
 
   def send_status(user)
     @courses = user.courses
-    status_mail = mail to: user.email, subject: "Your status"
+    status_mail = mail to: user.email, subject: 'Your status'
     status_mail.deliver
   end
 
@@ -25,8 +21,18 @@ class Mailer < ActionMailer::Base
 
   def send_enrollment_confirmation(user, course)
     code = rand(1000...9999)
-    @enrollment  = user.enrollments.create course: course, code: code, user: user
-    confirmation_mail = mail to: user.email, subject: "Confirm your enrollment in " + course.title
+    @enrollment  = user.enrollments
+      .create course: course, code: code, user: user
+    confirmation_mail = mail(
+      to: user.email,
+      subject: 'Confirm your enrollment in ' + course.title)
     confirmation_mail.deliver
+  end
+
+  def send_welcome(user, course)
+    @course = course
+    welcome_mail = mail to: user.email, subject: 'Welcome to ' + course.title
+    welcome_mail.deliver
+    send_next_lesson user, course
   end
 end
