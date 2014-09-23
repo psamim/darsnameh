c5a36ddd11d3e11b8d54cfaa059fa3b2d056283b
Samim Pezeshki <p.samim@gmail.com>
2014-09-23 10:35:50 +0330

    Refactor mail receiver, add enrollment confirmation

--- app/controllers/mail_reciever_controller.rb
+++ app/controllers/mail_reciever_controller.rb
@@ -2,44 +2,57 @@ class MailRecieverController < ApplicationController
   before_action :set_user, only: :on_incoming_email
   before_action :set_command, only: :on_incoming_email
   attr_accessor :user, :command
+  layout false
 
   def on_incoming_email
-    user.save
-    case command
-      when "status"
-        render plain: "Status mail sent to  " + user.email + " with ID " + user.id.to_s
-        send_status_mail
-    else
-      course = Course.find_by_email command
-      if course
-        render plain: "Confirmation code for " + course.id.to_s + " sent to " + user.email
-        send_enrollment_confirmation_mail course
-      else
-        render plain: command
-        send_command_not_found_mail
-      end
+    if command == 'status' && user
+      send_status_mail
+      return
     end
+
+    # See if it is a enrollment confirmation mail
+    code = /\d{4}/.match(email_params['stripped-text']).to_s || 99_999
+    enrollment = Enrollment.where(code: code, user: user).first
+    if enrollment # && !enrollment.confirmed
+      enrollment.confirmed = true
+      enrollment.save
+      send_welcome_mail user, enrollment.course
+      return
+    end
+
+    # See if it is a course enrollment mail
+    course = Course.find_by_email command
+    if course
+      send_enrollment_confirmation_mail user, course
+      return
+    end
+
+    send_command_not_found_mail
+    render plain: command + ' not found'
   end
 
   private
+
   def email_params
-    params.permit :sender, :recipient, :subject, "body-plain", "stripped-text"
+    params.permit :sender, :recipient, :subject, 'body-plain', 'stripped-text'
   end
 
   def set_user
     self.user = User.find_by_email email_params[:sender]
-    unless  self.user
-      self.user = User.new
-      self.user.email = email_params[:sender]
-    end
+    return if user
+    self.user = User.new
+    user.email = email_params[:sender]
+    user.save
   end
 
   def set_command
-    self.command= /.+(?=@)/.match(email_params[:recipient]).to_s
+    self.command = /.+(?=@)/.match(email_params[:recipient]).to_s
   end
 
-  def send_enrollment_confirmation_mail(course)
+  def send_enrollment_confirmation_mail(user, course)
     ConfirmationWorker.perform_async user.id, course.id
+    render plain:
+    'Confirmation code for ' + course.id.to_s + ' sent to ' + user.email
   end
 
   def send_command_not_found_mail
@@ -47,5 +60,13 @@ class MailRecieverController < ApplicationController
 
   def send_status_mail
     StatusWorker.perform_async user.id
+    render plain:
+    'Status mail sent to  ' + user.email + ' with ID ' + user.id.to_s
+  end
+
+  def send_welcome_mail(user, course)
+    WelcomeWorker.perform_async user.id, course.id
+    render plain:
+    'Welcome mail sent to  ' + user.email + ' with ID ' + user.id.to_s
   end
 end
