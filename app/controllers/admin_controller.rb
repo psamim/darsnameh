70333780ea785e1674e3f329629cc36880156cae
Samim Pezeshki <p.samim@gmail.com>
2014-09-24 18:50:07 +0330

    Modify change password for admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,9 +4,9 @@ class AdminController < ApplicationController
     @admin = Admin.find session[:admin_id]
     return unless admin_params[:current_password]
 
-    if @admin.authenticate admin_params[:current_password] and
-        admin_params[:new_password] == admin_params[:password_confirmation]
+    if @admin.authenticate admin_params[:current_password]
       @admin.password = admin_params[:new_password]
+      @admin.password_confirmation = admin_params[:password_confirmation]
       redirect_to courses_path and flash[:notice] = 'Password changed' if @admin.save
     end
   end
