315b2e8b1d7b0c37d199be1d448db6ea8f4e4bc7
Samim Pezeshki <p.samim@gmail.com>
2014-10-03 17:44:38 +0330

    Add course minitest

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not create course without title' do
+    course_attributes = attributes_for(:course)
+    course_attributes[:email] = nil
+    assert Course.new(course_attributes).invalid?(:email)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'factory_girl_rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
+  # fixtures :all
+  include FactoryGirl::Syntax::Methods
   # Add more helper methods to be used by all tests here...
 end
