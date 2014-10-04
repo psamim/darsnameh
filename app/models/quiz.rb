fc3e8eda96dd07619ec507e149e4d8f07b805341
Samim Pezeshki <p.samim@gmail.com>
2014-10-04 19:29:18 +0330

    Add generate_secret method to quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -7,12 +7,15 @@ class Quiz < ActiveRecord::Base
   end
 
   def self.create_quiz(user, lesson)
-    secret = Digest::SHA1.hexdigest Time.new.to_f.to_s + user.id.to_s
     quiz = Quiz.create(
                        user: user,
                        lesson: lesson,
-                       secret: secret,
+                       secret: generate_secret(user),
                        expire: 2.day.from_now)
     quiz
+  end
+
+  def self.generate_secret(user)
+    Digest::SHA1.hexdigest Time.new.to_f.to_s + user.id.to_s
   end
 end
