8cb05cbf254d3a57d9aca4a17ba24a61e49f536f
Samim Pezeshki <p.samim@gmail.com>
2014-10-05 23:04:28 +0330

    Remove tests, use specs instead

--- test/helpers/access_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class AccessHelperTest < ActionView::TestCase
-end
--- test/models/course_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CourseTest < ActiveSupport::TestCase
-  test 'should not create course without title' do
-    course_attributes = attributes_for(:course)
-    course_attributes[:email] = nil
-    assert Course.new(course_attributes).invalid?(:email)
-  end
-end
